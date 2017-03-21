200281f9bab55f536ca92cdf4d0507109023e83a
hassam <hassamsheikh1@gmail.com>
2017-03-21 19:08:02 -0400

    tracking for multiple objects added

--- ball_tracking.py
+++ ball_tracking.py
@@ -14,24 +14,24 @@ import cv2
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video",
 	help="path to the (optional) video file")
-ap.add_argument("-b", "--buffer", type=int, default=64,
+ap.add_argument("-b", "--buffer", type=int, default=10,
 	help="max buffer size")
 args = vars(ap.parse_args())
 
 # define the lower and upper boundaries of the "green"
 # ball in the HSV color space, then initialize the
 # list of tracked points
-color_range ={'green': {'lower': (29, 86, 6), 'upper': (64, 255, 255)}}
-color_range ={'red': {'lower': (153, 0, 0), 'upper': (255, 230, 230)}}
-greenUpper = (255, 230, 230)
-greenLower = (153, 0, 0)
-pts  	   = deque(maxlen=args["buffer"])
-temp_pts   = deque(maxlen=args["buffer"])
+color_range 	= {'green': {'lower': (29, 86, 6), 'upper': (64, 255, 255)},
+				  'red':    {'lower': (153, 0, 0), 'upper': (255, 230, 230)}}
+tracking_points = {}
+
+for color in color_range:
+	tracking_points[color] = deque(maxlen=args["buffer"])
 
 # if a video path was not supplied, grab the reference
 # to the webcam
 if not args.get("video", False):
-	camera = cv2.VideoCapture(1)
+	camera = cv2.VideoCapture(0)
 
 # otherwise, grab a reference to the video file
 else:
@@ -50,55 +50,30 @@ while True:
 	# resize the frame, blur it, and convert it to the HSV
 	# color space
 	frame = imutils.resize(frame, width=600)
-	# blurred = cv2.GaussianBlur(frame, (11, 11), 0)
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-	# construct a mask for the color "green", then perform
-	# a series of dilations and erosions to remove any small
-	# blobs left in the mask
-	mask = cv2.inRange(hsv, greenLower, greenUpper)
-	mask = cv2.erode(mask, None, iterations=2)
-	mask = cv2.dilate(mask, None, iterations=2)
-
-	# find contours in the mask and initialize the current
-	# (x, y) center of the ball
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)[-2]
-	center = None
-
-	# only proceed if at least one contour was found
-	if len(cnts) > 0:
-		# find the largest contour in the mask, then use
-		# it to compute the minimum enclosing circle and
-		# centroid
-		c = max(cnts, key=cv2.contourArea)
-		((x, y), radius) = cv2.minEnclosingCircle(c)
-		M = cv2.moments(c)
-		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-
-		# only proceed if the radius meets a minimum size
-		if radius > 10:
-			# draw the circle and centroid on the frame,
-			# then update the list of tracked points
-			cv2.circle(frame, (int(x), int(y)), int(radius),
-				(0, 255, 255), 2)
-			cv2.circle(frame, center, 5, (0, 0, 255), -1)
-			pts.appendleft(center)
-		else:
-			pts = deque(maxlen=args["buffer"])
-
-
-	# update the points queue
-	print(temp_pts == pts)
-	if temp_pts != pts:
-		len(pts)
-		for i in xrange(1, len(pts)):
-			if pts[i - 1] is None or pts[i] is None:
+	for color in color_range:
+		mask 				   = cv2.inRange(hsv, color_range[color]['lower'], color_range[color]['upper'])
+		mask 				   = cv2.erode(mask, None, iterations=2)
+		mask 				   = cv2.dilate(mask, None, iterations=2)
+		cnts 				   = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+		if len(cnts) > 0:
+			c = max(cnts, key=cv2.contourArea)
+			((x, y), radius) = cv2.minEnclosingCircle(c)
+			M = cv2.moments(c)
+			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+			if radius > 10:
+				cv2.circle(frame, (int(x), int(y)), int(radius),
+					   	  (0, 255, 255), 2)
+				cv2.circle(frame, center, 5, (0, 0, 255), -1)
+				tracking_points[color].appendleft(center)
+			else:
+				tracking_points[color] = deque(maxlen=args["buffer"])
+		for i in xrange(1, len(tracking_points[color])):
+			if tracking_points[color][i - 1] is None or tracking_points[color][i] is None:
 				continue
 			thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-			cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-		temp_pts = deepcopy(pts)
-	# show the frame to our screen
+			cv2.line(frame, tracking_points[color][i - 1], tracking_points[color][i], (0, 0, 255), thickness)
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
 
